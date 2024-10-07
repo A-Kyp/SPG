02e982b191da1337abe7a40831234cb16773c598
Chiper Alexandra-Diana <92289584+A-Kyp@users.noreply.github.com>
2024-10-07 13:18:53 +0300

    Update lab1 FragmentShader.glsl

--- lab1/shaders/FragmentShader.glsl
+++ lab1/shaders/FragmentShader.glsl
@@ -13,8 +13,10 @@ layout(location = 0) out vec4 out_color;
 void main()
 {
     // TODO(student): Apply the texture
+    vec4 color = texture2D(texture_1, texture_coord);
 
     // TODO(student): Discard when alpha component < 0.75
 
-    out_color = vec4(0.419, 0.584, 0.250, 0);
+    //out_color = vec4(0.419, 0.584, 0.250, 0);
+    out_color = color;
 }
